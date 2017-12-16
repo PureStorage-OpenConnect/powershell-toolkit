aeb8aace181b46ea3da86cbd064a2ff1cdb345e7
barkz <barkz@purestorage.com>
2017-12-15 21:13:19 -0800

    Clean-up

--- Installer/scripts/uncompress.ps1
+++
@@ -1,9 +0,0 @@
-param(
-	[Parameter(Mandatory=$true)] [string] $Source,
-	[Parameter(Mandatory=$true)] [string] $Destination
-)
-
-[Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
-$Compression = [System.IO.Compression.CompressionLevel]::Optimal
-
-[System.IO.Compression.ZipFile]::ExtractToDirectory($Source,$Destination)
